6cca79706f980473f7cda7f0fc6c201828957b98
lnbatioco <LoryleenBatioco@gmail.com>
2017-12-30 11:50:17 -0800

    declare member variables and link them to layout views

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    private Button mTopButton;
+    private Button mBottomButton;
+    private TextView mStoryTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +22,26 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Destini", "TOP BUTTON clicked!");
+            };
+        });
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Destini", "BOTTOM BUTTON clicked!");
+            }
+        });
 
 
     }
